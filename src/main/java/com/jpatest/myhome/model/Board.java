59e6618de5e015b04401e7826ef8ca75ed68629e
luke-jeong <luke@catious.io>
2021-08-23 10:59:53 +0900

    study Jpa

---
+++ src/main/java/com/jpatest/myhome/model/Board.java
@@ -0,0 +1,45 @@
+package com.jpatest.myhome.model;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하입니다.")
+    private String title;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
